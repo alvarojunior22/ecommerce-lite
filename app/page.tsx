f0164ffbef85102ba4079f810d829f7e487d0396
Elias-Cardenas-Coder <elias.cardenas0501@gmail.com>
2025-11-10 08:36:23 -0500

    feat: renderizado inicial de productos

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-
+import ProductList from "./components/ProductList/ProductList";
 
 function page() {
   return (
-    <div>
-      <h1>hola</h1>
-    </div>
-  )
+    <main className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center mb-8">Nuestros Productos</h1>
+      <ProductList />
+    </main>
+  );
 }
 
-export default page
+export default page;
